13a660c30d906556551f5a7b606b031b80332d64
MavericksBijan <bijanmukherjee05@gmail.com>
2019-09-05 10:28:17 +0530

    DeloitteExercise

---
+++ BusCompanyModel.java
@@ -0,0 +1,58 @@
+package com.deloitte;
+
+
+   public class BusCompanyModel {
+	
+	/**
+	 * 
+	 */
+	private int customerId;
+	private String customerType;
+	private String customerName;
+	private String favRoute;
+	private String route;
+	private double price;
+	
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerType() {
+		return customerType;
+	}
+	public void setCustomerType(String customerType) {
+		this.customerType = customerType;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public String getFavRoute() {
+		return favRoute;
+	}
+	public void setFavRoute(String favRoute) {
+		this.favRoute = favRoute;
+	}
+	public String getRoute() {
+		return route;
+	}
+	public void setRoute(String route) {
+		this.route = route;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+
+}
---
+++ BusJourney.java
@@ -0,0 +1,42 @@
+package com.deloitte;
+
+public class BusJourney {
+	
+	private int customerId;
+	private String journey_start_date_time;
+	private String routeName;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getJourney_start_date_time() {
+		return journey_start_date_time;
+	}
+	public void setJourney_start_date_time(String journey_start_date_time) {
+		this.journey_start_date_time = journey_start_date_time;
+	}
+	public String getRouteName() {
+		return routeName;
+	}
+	public void setRouteName(String routeName) {
+		this.routeName = routeName;
+	}
+	
+	
+	
+	
+	
+	
+
+}
